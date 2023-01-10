e42cd55e57a6ab21b32b90412cb85cf6d974ee80
FreetooNe <Freepandas2000@gmail.com>
2023-01-10 23:46:16 +0200

    [Homeword 14] Added new tests

--- test_login_user.py
+++ test_login_user.py
@@ -9,18 +9,18 @@ from selenium.webdriver.common.by import By
 class TestStartPage:
     log = logging.getLogger("[StartPage]")
 
-    """
-           - Pre-conditions:
-               1 - Open start page
-           - Steps:
-               2 - Fill login
-               3 - Fill password
-               4 - Click on SignIn button
-               5 - Verify error
-           """
-
-    # step 1
     def test_start_page(self):
+        """
+                    -Description: Positive TEST with correct Password and Login
+                       - Pre-conditions:
+                           1 - Open start page
+                       - Steps:
+                           2 - Fill login
+                           3 - Fill password
+                           4 - Click on SignIn button
+                           5 - Verify error
+                       """
+        # Open start page
         driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
         driver.get("https://qa-complexapp.onrender.com")
         self.log.info("Start page was opened")
@@ -29,27 +29,166 @@ class TestStartPage:
         # fill login
         login_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
         login_field.clear()
+        login_field.send_keys("Tverdokhlib")
+        self.log.info("filled login")
+        sleep(1)
+
+        # fill password
+        password_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
+        password_field.clear()
+        password_field.send_keys("Testtesttest")
+        self.log.info("filled password]")
+        sleep(1)
+
+        # click on button signIn
+        signin_button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
+        signin_button.click()
+        self.log.info("click button sign in")
+        sleep(1)
+
+        # find for a button SignIn
+        sign_out = driver.find_element(by=By.XPATH, value=".//button[@class = 'btn btn-sm btn-secondary']")
+        self.log.info("Button sign out was found")
+
+        # checking for an error message
+        assert sign_out, f"Button Sign_Out not found {sign_out}"
+
+        driver.close()
+
+        """
+            -Description: Negative TEST with incorrect Password and Login
+               - Pre-conditions:
+                   1 - Open start page
+               - Steps:
+                   2 - Fill login
+                   3 - Fill password
+                   4 - Click on SignIn button
+                   5 - Verify error
+               """
+        # Open start page
+        driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
+        driver.get("https://qa-complexapp.onrender.com")
+        self.log.info("Start page was opened")
+        sleep(1)
+
+        # fill incorrect login
+        login_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
+        login_field.clear()
         login_field.send_keys(f"email{randint(1, 200)}.com")
         self.log.info("filled loggin")
         sleep(1)
 
-        # fill password
+        # fill incorrect password
         password_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
         password_field.clear()
         password_field.send_keys(f"{randint(1000, 2000)}")
         self.log.info("filled password]")
         sleep(1)
 
+        # click on button signIn
         signin_button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
         signin_button.click()
         self.log.info("click button sign in")
         sleep(1)
 
+        # find for a button SignIn
         error_message = driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger text-center']")
-        assert error_message.text == 'Error'
         self.log.info("Error message was verified")
+
+        # checking for an error message
+        assert error_message.text == 'Error'
 
         driver.close()
 
-    class Registred_new_user:
-        log = logging.getLogger("[StartPage]")
+        """
+               -Description: Negative TEST with correct Password and incorrect Login
+                  - Pre-conditions:
+                      1 - Open start page
+                  - Steps:
+                      2 - Fill incorrect login
+                      3 - Fill correct password
+                      4 - Click on SignIn button
+                      5 - Verify error
+                  """
+
+        # Open start page
+
+        driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
+        driver.get("https://qa-complexapp.onrender.com")
+        self.log.info("Start page was opened")
+        sleep(1)
+
+        # Fill incorrect login
+        login_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
+        login_field.clear()
+        login_field.send_keys("IncorrectLogin")
+        self.log.info("filled login")
+        sleep(1)
+
+        # Fill correct password
+        password_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
+        password_field.clear()
+        password_field.send_keys("Testtesttest")
+        self.log.info("filled password]")
+        sleep(1)
+
+        # click on button signIn
+        signin_button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
+        signin_button.click()
+        self.log.info("click button sign in")
+        sleep(1)
+
+        # find error message
+        error_message = driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger text-center']")
+        self.log.info("Error message was verified")
+
+        # checking for an error message
+        assert error_message.text == 'Error'
+
+        # end test
+        driver.close()
+
+        """
+                      -Description: Negative TEST with incorrect Password and correct Login
+                         - Pre-conditions:
+                             1 - Open start page
+                         - Steps:
+                             2 - Fill incorrect login
+                             3 - Fill correct password
+                             4 - Click on SignIn button
+                             5 - Verify error
+                         """
+
+        driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
+        driver.get("https://qa-complexapp.onrender.com")
+        self.log.info("Start page was opened")
+        sleep(1)
+
+        # Fill correct login
+        login_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
+        login_field.clear()
+        login_field.send_keys("Tverdokhlib")
+        self.log.info("filled login")
+        sleep(1)
+
+        # Fill incorrect password
+        password_field = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
+        password_field.clear()
+        password_field.send_keys("1233213123")
+        self.log.info("filled password]")
+        sleep(1)
+
+        # click on button signIn
+        signin_button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
+        signin_button.click()
+        self.log.info("click button sign in")
+        sleep(1)
+
+        # Find  error massage
+        error_message = driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger text-center']")
+        self.log.info("Error message was verified")
+
+        # checking for an error message
+        assert error_message.text == 'Error'
+
+        driver.close()
--- test_register_user.py
+++ test_register_user.py
@@ -10,6 +10,7 @@ class TestRegisterUser:
     log = logging.getLogger("[Register form NEW user: ]")
 
     """
+           -Description: Positive TEST 
            - Pre-conditions:
                1 - Open start page
            - Steps:
@@ -25,6 +26,7 @@ class TestRegisterUser:
         driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
         driver.get("https://qa-complexapp.onrender.com")
         driver.maximize_window()
+        print(" ========= Positive TEST ========= ")
         self.log.info("Start page was opened")
         sleep(1)
 
@@ -53,7 +55,75 @@ class TestRegisterUser:
         sign_button.click()
         self.log.info("The button has been pressed")
 
+        # find button Sign_Out
         sign_out = driver.find_element(by=By.XPATH, value=".//button[@class = 'btn btn-sm btn-secondary']")
         self.log.info("Button sign out was found")
 
+        # check for button Sign_Out
         assert sign_out, f"Button Sign_Out not found {sign_out}"
+
+        driver.close()
+
+        """
+                   -Description: Checking for validation of the email field 
+                   - Pre-conditions:
+                       1 - Open start page
+                   - Steps:
+                       2 - Fill email
+                       3 - Verify error
+                   """
+
+        # STEP 1 - Open start page
+        driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
+        driver.get("https://qa-complexapp.onrender.com")
+        driver.maximize_window()
+        print(" =========  TEST - Checking for validation of the email field ========= ")
+        self.log.info("Start page was opened")
+        sleep(1)
+
+        # STEP 2 - Fill incorrect email
+        field_incorrect_email = driver.find_element(by=By.XPATH, value=".//input[@id = 'email-register']")
+        field_incorrect_email.clear()
+        field_incorrect_email.send_keys(f"TestEmail{randint(0, 1000)}")
+        self.log.info("Incorrect email was entered")
+
+        # STEP 3 - found validation massage
+        validation_massage_email = driver.find_element(by=By.XPATH, value="//*[@id='registration-form']/div[2]/div")
+        self.log.info("validation massage was found")
+
+        # check for button Sign_Out
+        assert validation_massage_email
+
+        driver.close()
+
+        """
+                          -Description: Checking for validation of the password field 
+                          - Pre-conditions:
+                              1 - Open start page
+                          - Steps:
+                              2 - Fill email
+                              3 - Verify error
+                          """
+
+        # STEP 1 - Open start page
+        driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
+        driver.get("https://qa-complexapp.onrender.com")
+        driver.maximize_window()
+        print(" =========  TEST - Checking for validation of the password field ========= ")
+        self.log.info("Start page was opened")
+        sleep(1)
+
+        # STEP 2 - Fill password not more then 12 symbols
+        user_password_field = driver.find_element(by=By.XPATH, value=".//input[@id = 'password-register']")
+        user_password_field.clear()
+        user_password_field.send_keys(f"{randint(100, 10000)}")
+        self.log.info("Incorrect Password has been entered")
+
+        # STEP 3 - Verify validation_massage
+        validation_massage_password = driver.find_element(by=By.XPATH, value='//*[@id="registration-form"]/div[3]/div')
+        self.log.info("Verify massage was found")
+
+        # check for button Sign_Out
+        assert validation_massage_password
+
+        driver.close()
