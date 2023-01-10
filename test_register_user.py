39aef3e06af3832f0a9bd29d2eea701c821a2ffc
FreetooNe <Freepandas2000@gmail.com>
2023-01-10 20:31:32 +0200

    [Lesson 14] Updates test files

--- test_register_user.py
+++ test_register_user.py
@@ -21,32 +21,32 @@ class TestRegisterUser:
            """
 
     def test_start_page(self):
-        """STEP 1 - Open start page"""
+        # STEP 1 - Open start page
         driver = webdriver.Chrome(executable_path="/Users/ablofon8/PycharmProjects/QaComplex/chromedriver")
         driver.get("https://qa-complexapp.onrender.com")
         driver.maximize_window()
         self.log.info("Start page was opened")
         sleep(1)
 
-        """ STEP 2 - Fill username """
+        # STEP 2 - Fill username
         user_name_field = driver.find_element(by=By.XPATH, value=".//input[@id = 'username-register']")
         user_name_field.clear()
         user_name_field.send_keys(f"Dima{randint(0, 1000)}")
         self.log.info("Username has been entered")
 
-        """ STEP 3 - Fill random EMAIL """
+        # STEP 3 - Fill random EMAIL
         user_email_field = driver.find_element(by=By.XPATH, value=".//input[@id = 'email-register']")
         user_email_field.clear()
         user_email_field.send_keys(f"TestEmail{randint(0, 1000)}@gmail.com")
         self.log.info("Email has been entered")
 
-        """ STEP 4 - Fill random password """
+        #  STEP 4 - Fill random password
         user_password_field = driver.find_element(by=By.XPATH, value=".//input[@id = 'password-register']")
         user_password_field.clear()
         user_password_field.send_keys(f"{randint(1000000000000, 10000000000000)}")
         self.log.info("Password has been entered")
 
-        """ STEP 5 - Click on button "Sign up for OurApp" """
+        # STEP 5 - Click on button "Sign up for OurApp"
         sign_button = driver.find_element(by=By.XPATH, value=".//button[@type='submit']")
         sign_button.click()
         sleep(5)
